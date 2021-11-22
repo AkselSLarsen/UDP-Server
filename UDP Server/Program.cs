dd8e05783d72a7e6ae093310ca819ac195471966
AkselSLarsen <akse2669@edu.easj.dk>
2021-11-22 11:18:41 +0100

    "UDP Server Program" Task er færdig

--- UDP Server/Program.cs
+++ UDP Server/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace UDP_Server {
     public class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+        private static IPAddress ip = IPAddress.Any;
+        private static int port = 7161;
+
+        public static void Main(string[] args) {
+            while(true) {
+                string data = ReadFromPort();
+                if (data != null) {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+        public static string ReadFromPort() {
+            try {
+                UdpClient socket = new UdpClient();
+                socket.Client.Bind(new IPEndPoint(ip, port));
+
+                IPEndPoint from = null;
+                byte[] data = socket.Receive(ref from);
+                string dataString = Encoding.UTF8.GetString(data);
+
+                return dataString;
+            } catch(Exception e) {
+                //Console.Error.WriteLine(e);
+            }
+            return null;
         }
     }
 }
