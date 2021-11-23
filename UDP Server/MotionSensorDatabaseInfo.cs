d9dfb87fc73ae5777b29b78c56d2423ca2935bb6
AkselSLarsen <akse2669@edu.easj.dk>
2021-11-23 09:10:40 +0100

    Trying to make the connectionstring secret

--- UDP Server/MotionSensorDatabaseInfo.cs
+++ UDP Server/MotionSensorDatabaseInfo.cs
@@ -2,7 +2,7 @@
     public class MotionSensorDatabaseInfo : IDataBaseInfo {
         public string ConnectionString {
             get {
-                return "Server=tcp:emilzealanddb.database.windows.net,1433;Initial Catalog=ParkeringsDataDb;Persist Security Info=False;User ID=emiladmin;Password=Sql12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                return "CONNECTIONKEYFOREMILSDATABASE";
             }
         }
 
